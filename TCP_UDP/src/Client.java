57def047ef5ccfbfd84407fe148df80d4935d37f
Tijs Van Alphen <tijs.van.alphen@hotmail.com>
2021-04-20 15:50:15 +0200

    TCP/UDP

--- TCP_UDP/src/Client.java
+++ TCP_UDP/src/Client.java
@@ -1,9 +1,21 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client
 {
     public static void main(String[] args) throws IOException {
-        Socket s = new Socket("127.0.0.1",80);
+        Socket s = new Socket("127.0.0.1",5000);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.flush();
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("Server : "+ str);
     }
 }
--- TCP_UDP/src/Server.java
+++ TCP_UDP/src/Server.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,5 +12,15 @@ public class Server
         Socket s = ss.accept();
 
         System.out.println("Client connected");
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("Client : "+ str);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("yes");
+        pr.flush();
     }
 }
