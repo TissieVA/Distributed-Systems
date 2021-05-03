61900b3f3b3716153b0b3456ca71d8974518be86
Wocco <vanuytselwout@gmail.com>
2021-05-03 09:55:50 +0200

    httprequester progress

--- Client/src/main/java/be/ua/fti/ei/HttpRequester.java
+++ Client/src/main/java/be/ua/fti/ei/HttpRequester.java
@@ -3,18 +3,46 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
+import java.net.http.*;
+
+
+
+
+
+
 
 public class HttpRequester {
     //Deze klas maakt requests voor u naar een https server, als je een id wilt weten kan je het gewoon aanvragen, je kan andere files zoeken
     public void httprequestPOST(String url, String body){//string body json object
-       /* HttpRequest request = HttpRequest.newBuilder()
+        HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
-                .header("Content-Type", "application/json")*/
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
 
+        var client = HttpClient.newHttpClient();
+        try {
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    public void httprequestGet(String url){
+
+
+    public void httprequestGet(String url){//Get and prints to the screen
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        try {
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
