64a2806fd5820e901e7db1fd1f91ed4c351c8cae
Wocco <vanuytselwout@gmail.com>
2021-05-03 10:28:04 +0200

    httprequester finished needs to be tested

--- Client/src/main/java/be/ua/fti/ei/HttpRequester.java
+++ Client/src/main/java/be/ua/fti/ei/HttpRequester.java
@@ -1,19 +1,25 @@
 package be.ua.fti.ei;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.*;
 
-
-
-
-
-
-
 public class HttpRequester {
-    //Deze klas maakt requests voor u naar een https server, als je een id wilt weten kan je het gewoon aanvragen, je kan andere files zoeken
-    public void httprequestPOST(String url, String body){//string body json object
+
+
+    //we are using Gson, Gson is a java library that can convert Java Objects into their JSON representation,
+    // it can also convert a JSON string into a java object. Exactly what we need. More info on https://github.com/google/gson
+    private static Gson gsonConvertor=new Gson();//has to be static cause otherwise can't acces it from a static context.
+
+
+
+
+
+    //when we don't get anything back we can use this
+    public static void httprequestPOST(String url, String body){//string body json object
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Content-Type", "application/json")
@@ -30,8 +36,39 @@ public class HttpRequester {
         }
     }
 
+    //when we don't get anything back we can use this
+    public static Object httprequestPOST(String url, String body,Class cl){//string body json object
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
 
-    public void httprequestGet(String url){//Get and prints to the screen
+        var client = HttpClient.newHttpClient();
+        try {
+            var res=client.send(request, HttpResponse.BodyHandlers.ofString());
+            if(res.statusCode()==200){
+                return gsonConvertor.fromJson(res.body(), cl);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+
+    //GET that doesn't get anything back
+    public static void httprequestGet(String url){
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -43,6 +80,33 @@ public class HttpRequester {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
+
+
+    //GET that doesn't get anything back
+    public static Object httprequestGet(String url,Class cl){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        try {
+            var res = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (res.statusCode()==200){//if the status code is 200 it means that the response is OK
+                //we get a JSON response back so now we need to convert it to the class that was given as a parameter. See JSON conversion function(static)
+                return gsonConvertor.fromJson(res.body(),cl);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+
+
+
+
+
 }
