b18f49a6fb5a41f8b76c6e77397ef37cfd12c1e1
Wocco <vanuytselwout@gmail.com>
2021-05-10 09:48:38 +0200

    update

--- Project/Client/src/main/java/be/ua/fti/ei/FileTransfer.java
+++ Project/Client/src/main/java/be/ua/fti/ei/FileTransfer.java
@@ -2,19 +2,60 @@ package be.ua.fti.ei;
 
 import org.apache.catalina.Server;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class FileTransfer {
 
 
-    public static void transferFile(ServerSocket clientsocket, File file) throws IOException {
+    public static void transferFileSender(String ipaddres,int port, File file, InetAddress ipb) throws IOException
+    {
         //initialize the socket
-        Socket socket= clientsocket.accept();
-        
+        ServerSocket clientsocket=new ServerSocket(port);
+        Socket socket = clientsocket.accept();
+        InetAddress IA = InetAddress.getByName("localhost");
+        //FIlE
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        //get socket output stream
+        OutputStream os = socket.getOutputStream();
+        //put the file into an array
+        byte[] contents;
+        long fileLength = file.length();
+        long current=0;
+        while(current!=fileLength){
+            int size=10000;
+            if(fileLength-current>=size){
+                current+=size;
+            }
+            else{
+                size = (int)(fileLength-current);
+                current = fileLength;
+            }
+            contents = new byte[size];
+            bis.read(contents,0,size);
+            os.write(contents);
+        }
+        os.flush();
+        //filetransfer is finished
+        socket.close();
+        clientsocket.close();
+    }
+
+
+    public static void transferFileReciever(String ipaddress,int port) throws IOException
+    {
+        //initialize socket
+        Socket socket = new Socket(InetAddress.getByName("localhost"),port);
+        byte[] contents =new byte[10000];
+        //file outputstrem to the output path
+        FileOutputStream fileOutputStream=new FileOutputStream("");
 
     }
+
+
 
 }
