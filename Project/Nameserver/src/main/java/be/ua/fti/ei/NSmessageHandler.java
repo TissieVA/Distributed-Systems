d60f08405623a9b31ef5972d6ffac615470163c3
Asif Wasefi <asifwasefi@yahoo.com>
2021-05-10 12:21:58 +0200

    NSmessagehandler created

---
+++ Project/Nameserver/src/main/java/be/ua/fti/ei/NSmessageHandler.java
@@ -0,0 +1,45 @@
+package be.ua.fti.ei;
+
+
+import be.ua.fti.ei.sockets.MulticastSocketServer;
+import be.ua.fti.ei.sockets.*;
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class NSmessageHandler implements MessageHandler
+{
+    private static final Logger logger = LoggerFactory.getLogger(be.ua.fti.ei.sockets.MulticastSocketServer.class);
+    private static Gson gson;
+    private be.ua.fti.ei.sockets.MulticastSocketServer mss;
+
+    @Override
+    public void setServer(MulticastSocketServer mss)
+    {
+        this.mss = mss;
+    }
+
+    @Override
+    public void parse(SocketBody sb, String msg, String ip, int port)
+    {
+        if(sb.getType().equals("find"))
+        {
+            sendNS();
+        }
+
+
+    }
+
+    public void sendNS()
+    {
+        gson = new Gson();
+
+        NameServerResponseBody nsb = new NameServerResponseBody(8080);
+
+        String msg = gson.toJson(nsb);
+
+        this.mss.sendMessage(msg);
+
+    }
+}
