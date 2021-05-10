702d9410e17b684f6f3956f991ca0677bc288b8d
Wocco <56872678+Wocco@users.noreply.github.com>
2021-05-10 13:32:35 +0200

    Added automated loading (#18)
    * start
    
    * update
    
    * Added automated loading
    
    * gitignore added config

---
+++ Project/Client/src/main/java/be/ua/fti/ei/NodeConfig.java
@@ -0,0 +1,64 @@
+package be.ua.fti.ei;
+
+import com.google.gson.Gson;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class NodeConfig implements java.io.Serializable
+{
+    private String name;
+    private String ifName;
+    private int fileTransferPort;
+    private int multicastPort;
+
+    public NodeConfig() { }
+
+    public String getIfName()
+    {
+        return ifName;
+    }
+
+    public void setIfName(String ifName)
+    {
+        this.ifName = ifName;
+    }
+
+    public int getFileTransferPort()
+    {
+        return fileTransferPort;
+    }
+
+    public void setFileTransferPort(int fileTransferPort)
+    {
+        this.fileTransferPort = fileTransferPort;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMulticastPort()
+    {
+        return multicastPort;
+    }
+
+    public void setMulticastPort(int multicastPort)
+    {
+        this.multicastPort = multicastPort;
+    }
+
+    public static NodeConfig load(String file) throws Exception
+    {
+        // Read file to string
+        Path filename = Path.of(file);
+        String content = Files.readString(filename);
+
+        Gson gson = new Gson();
+        return gson.fromJson(content, NodeConfig.class);
+    }
+}
