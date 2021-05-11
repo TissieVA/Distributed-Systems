0f412470131fe9e10297a8a0c5b92b2a16264c16
Tijs Van Alphen <tijs.van.alphen@hotmail.com>
2021-05-11 10:48:12 +0200

    Cleaning up

--- Project/Utils/src/main/java/be/ua/fti/ei/sockets/NextPreviousBody.java
+++ Project/Utils/src/main/java/be/ua/fti/ei/sockets/NextPreviousBody.java
@@ -3,17 +3,17 @@ package be.ua.fti.ei.sockets;
 
 import be.ua.fti.ei.sockets.PublishBody;
 
-public class NextPrevious extends PublishBody implements java.io.Serializable {
+public class NextPreviousBody extends PublishBody implements java.io.Serializable {
 
     private int previous,next,numberOfNodes;
 
-    public NextPrevious(int previous, int next, int numberOfNodes) {
+    public NextPreviousBody(int previous, int next, int numberOfNodes) {
         this.previous = previous;
         this.next = next;
         this.numberOfNodes = numberOfNodes;
     }
 
-    public NextPrevious() { }
+    public NextPreviousBody() { }
 
     public int getPrevious() {
         return previous;
