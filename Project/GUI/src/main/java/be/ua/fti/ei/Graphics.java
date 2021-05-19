59ba39949451106a99e977da84dd3d09df66e253
Wocco <vanuytselwout@gmail.com>
2021-05-19 16:03:27 +0200

    fixxed config loader

--- Project/GUI/src/main/java/be/ua/fti/ei/Graphics.java
+++ Project/GUI/src/main/java/be/ua/fti/ei/Graphics.java
@@ -23,9 +23,8 @@ public class Graphics
 
     public static void main(String args[]) throws Exception
     {
-        NSConfig nsConfig = new NSConfig();
-        NSConfig.load("config.json");
-
+        NSConfig nsConfig = NSConfig.load("config.json");
+        System.out.println("the port is:"+nsConfig.getHttpPort());
         JFrame frame = new JFrame("Distributed Systems");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
@@ -62,16 +61,16 @@ public class Graphics
         JPanel panelIp = new JPanel();
 
         JLabel labelName = new JLabel("Give the name of the node:");
-        JTextField tfName = new JTextField(16);
+        JTextField tfName = new JTextField(20);
 
         JLabel labelIp = new JLabel("Give the IP of the node:");
-        JTextField tfIP = new JTextField(16);
+        JTextField tfIP = new JTextField(20);
 
         JLabel labelport = new JLabel("Give the multicastport of the node:");
-        JTextField tfport = new JTextField(16);
+        JTextField tfport = new JTextField(20);
 
         JLabel labelportunicast = new JLabel("Give the unicastport of the node:");
-        JTextField unitf = new JTextField(16);
+        JTextField unitf = new JTextField(20);
 
         JButton send = new JButton("Send");
         send.addActionListener(new ActionListener()
@@ -94,6 +93,7 @@ public class Graphics
                 {
                     PublishBody publishBody = new PublishBody(textfieldName,null,textfieldIP,tfPort,tfunicastPort);
                     String json = gson.toJson(publishBody);
+                    System.out.println("what we are sending to the post:"+nsConfig.getIpAddress() + ":" + nsConfig.getHttpPort() + "/publish");
                     HttpRequester.POST( nsConfig.getIpAddress() + ":" + nsConfig.getHttpPort() + "/publish", json, NextPreviousBody.class);//need to add the ip of the nameserver and port
                 }
             }
