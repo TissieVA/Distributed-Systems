46fa63b5064b9e30ef8e20280a575a7049a19ee5
Tijs Van Alphen <tijs.van.alphen@hotmail.com>
2021-06-12 15:58:46 +0200

    Update neighbors when new node is published

--- Project/Nameserver/src/main/java/be/ua/fti/ei/nameserver/Controller.java
+++ Project/Nameserver/src/main/java/be/ua/fti/ei/nameserver/Controller.java
@@ -46,20 +46,19 @@ public class Controller
 
             return null;
 
-
+        NSmessageHandler.getInstance().updateNeighbours(Hasher.getHash(body.getHostname()),false);
         return Database.getInstance().getNeighbours(body.getHostname());
     }
 
     //The remove node is not yet fully finished, needs to implement the next and previous node
     @GetMapping("/remove/{nodeName}")
-    boolean removeNode(@PathVariable String nodeName)
+    void removeNode(@PathVariable String nodeName)
     {
         logger.info("Remove node request received");
         int hash = Hasher.getHash(nodeName);
 
-        NSmessageHandler.getInstance().updateNeighboursAfterDeletion(hash);
+        NSmessageHandler.getInstance().updateNeighbours(hash,true);
 
-        return Database.getInstance().removeNode(nodeName); // remove the node (works)
     }
 
 
